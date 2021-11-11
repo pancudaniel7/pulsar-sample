3bd89794b5d44ee654ec78870656587d655a86e6
pancudaniel7 <pancudaniel7@gmail.com>
2021-11-11 11:56:11 +0200

    Refactored dpuslar data location

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,15 +7,15 @@ namespace="default"
 topic="t1"
 topic2="o2"
 
-# container_id="$(docker ps | grep apachepulsar/pulsar:2.7.1 | cut -d" " -f1)" &&
-#     docker exec -it "$container_id" $pulsar_client_path/pulsar-admin tenants create $tenant &&
-#     echo "Created tenant: $tenant"
+container_id="$(docker ps | grep apachepulsar/pulsar:2.7.1 | cut -d" " -f1)" &&
+    docker exec -it "$container_id" $pulsar_client_path/pulsar-admin tenants create $tenant &&
+    echo "Created tenant: $tenant"
 
 # docker exec -it "$container_id" $pulsar_client_path/pulsar-admin namespaces create "$tenant/$namespace" &&
 #     echo "Created namespace: $namespace"
 
-# docker exec -it "$container_id" $pulsar_client_path/pulsar-admin schemas upload --filename "/pulsar/data/test.avro" "$tenant/$namespace/$topic" &&
-#     echo "Created schema: $tenant/$namespace/$topic"
+docker exec -it "$container_id" $pulsar_client_path/pulsar-admin schemas upload --filename "/pulsar/data/test.avro" "$tenant/$namespace/$topic" &&
+    echo "Created schema: $tenant/$namespace/$topic"
     
-# docker exec -it "$container_id" $pulsar_client_path/pulsar-admin schemas upload --filename "/pulsar/data/test.avro" "$tenant/$namespace/$topic" &&
-#     echo "Created schema: $tenant/$namespace/$topic2"
+docker exec -it "$container_id" $pulsar_client_path/pulsar-admin schemas upload --filename "/pulsar/data/test.avro" "$tenant/$namespace/$topic" &&
+    echo "Created schema: $tenant/$namespace/$topic2"
